c09a75f9aad99b09b1bb53d94a1f520e33e11ea7
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-05-17 16:37:39 +0700

    StackLayout & In Code

---
+++ MyStackLayout/MyStackLayout/MyStackLayout/StackLayoutinCode.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyStackLayout
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StackLayoutinCode : ContentPage
+    {
+        public StackLayoutinCode()
+        {
+            InitializeComponent();
+            Content = new StackLayout
+            {
+                BackgroundColor = Color.Aqua,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Orientation = StackOrientation.Horizontal,
+                Padding = new Thickness(20, 20, 30, 20),
+                Children =
+                {
+                    new Button{Text = "Button1"},
+                    new Button{Text = "Button2"},
+                    new Button{Text = "Button3"},
+                    new StackLayout
+                    {
+                        BackgroundColor = Color.Yellow,
+                        Children =
+                        {
+                            new Button{Text = "Button4"},
+                            new Button{Text = "Button5"}
+                        }
+
+                    }
+                }
+            };
+        }
+    }
+}
